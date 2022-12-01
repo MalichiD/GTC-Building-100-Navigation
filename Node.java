ed9e2b9eb40a6f19e07c81aa3ec5883db0d13da4
Malichi Hill <99568086+MalichiD@users.noreply.github.com>
2022-12-01 17:41:31 -0600

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,19 +1,30 @@
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class Node {
+public class Node implements Comparable<Node>{
     private String name;
     private ArrayList<Hall> adjacentHalls;
     private boolean visited;
     private Node previousNode;
-    private Integer distance;
+    private Integer distance = Integer.MAX_VALUE;
+
+    private Node northernNeighbor;
+    private Node southernNeighbor;
+    private Node easternNeighbor;
+    private Node westernNeighbor;
+
 
 
 
     public Node(String name){
         this.name = name;
-        adjacentHalls = new ArrayList<>();
+        this.adjacentHalls = new ArrayList<>();
+        this.northernNeighbor = null;
+        this.southernNeighbor = null;
+        this.easternNeighbor = null;
+        this.westernNeighbor = null;
     }
 
     public void addAdjacentHalls(Hall hall){this.adjacentHalls.add(hall);}
@@ -34,6 +45,27 @@ public class Node {
         this.adjacentHalls.add(hall4);
     }
 
+    public Destination exitDestination(int number, Hall parentHall){
+        Destination destination = new Destination(this.name, number, parentHall, this, 'C');
+        return destination;
+    }
+
+    public Node getNorthernNeighbor() {
+        return northernNeighbor;
+    }
+
+    public Node getSouthernNeighbor() {
+        return southernNeighbor;
+    }
+
+    public Node getEasternNeighbor() {
+        return easternNeighbor;
+    }
+
+    public Node getWesternNeighbor() {
+        return westernNeighbor;
+    }
+
     public String getName() {
         return name;
     }
@@ -44,6 +76,10 @@ public class Node {
 
     public Node getPreviousNode() {
         return previousNode;
+    }
+
+    public Integer getDistance() {
+        return distance;
     }
 
     public void setName(String name) {
@@ -62,7 +98,48 @@ public class Node {
         this.visited = visited;
     }
 
+    public void setNeighbors(Node northernNeighbor, Node southernNeighbor, Node easternNeighbor, Node westernNeighbor){
+        this.northernNeighbor = northernNeighbor;
+        this.southernNeighbor = southernNeighbor;
+        this.easternNeighbor = easternNeighbor;
+        this.westernNeighbor = westernNeighbor;
+    }
+
+    public void setNorthernNeighbor(Node northernNeighbor) {
+        this.northernNeighbor = northernNeighbor;
+    }
+
+    public void setSouthernNeighbor(Node southernNeighbor) {
+        this.southernNeighbor = southernNeighbor;
+    }
+
+    public void setEasternNeighbor(Node easternNeighbor) {
+        this.easternNeighbor = easternNeighbor;
+    }
+
+    public void setWesternNeighbor(Node westernNeighbor) {
+        this.westernNeighbor = westernNeighbor;
+    }
     public boolean isVisited() {
         return visited;
+    }
+
+    public void setDistance(Integer distance) {
+        this.distance = distance;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    @Override
+    public int compareTo(Node otherNode) {
+        return Integer.compare(this.distance, otherNode.getDistance());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
