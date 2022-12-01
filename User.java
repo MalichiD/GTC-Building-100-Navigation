0fecda051708398fbd4ab2b7e41ab2ae9da399e0
Malichi Hill <99568086+MalichiD@users.noreply.github.com>
2022-12-01 17:42:19 -0600

    Update User.java

--- User.java
+++ User.java
@@ -1,35 +1,73 @@
 public class User {
     private char direction;
-    private Node previousNode;
+
+    private char previousDirection;
     private Node currentNode;
+    private Node nextNode;
 
 
     public User(){
         this.direction = '0';
-        this.currentNode = null;
+        this.nextNode = null;
     }
 
-    public void setPreviousNode(Node previousNode) {
-        this.previousNode = previousNode;
+    public void setCurrentNode(Node currentNode) {
+        this.currentNode = currentNode;
     }
 
-    public void setCurrentNode(Node currentNode) {this.currentNode = currentNode;
+    public void setNextNode(Node nextNode) {this.nextNode = nextNode;
     }
 
+    public void setDirection(){
+        if (nextNode.equals(currentNode.getNorthernNeighbor())){
+            direction = 'N';
+        } else if (nextNode.equals(currentNode.getSouthernNeighbor())) {
+            direction = 'S';
+        } else if (nextNode.equals(currentNode.getEasternNeighbor())){
+            direction = 'E';
+        } else if (nextNode.equals(currentNode.getWesternNeighbor())){
+            direction = 'W';
+        }
+    }
     public void setDirection(char direction) {
         this.direction = direction;
     }
 
+    public void setPreviousDirection(char previousDirection) {
+        this.previousDirection = previousDirection;
+    }
 
     public char getDirection() {
         return direction;
+    }
+
+    public char getPreviousDirection() {
+        return previousDirection;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
     }
 
     public Node getCurrentNode() {
         return currentNode;
     }
 
-    public Node getPreviousNode() {
-        return previousNode;
+    public void printDirectionChange(){
+        if((previousDirection == 'N' && direction == 'E') || (previousDirection == 'E' && direction == 'S') || (previousDirection == 'S' && direction == 'W') || (previousDirection == 'W' && direction == 'N')) {
+
+            System.out.println("Turn right, ");
+
+        } else if ((previousDirection == 'N' && direction == 'W') || (previousDirection == 'W' && direction == 'S') || (previousDirection == 'S' && direction == 'E') || (previousDirection == 'E' && direction == 'N')) {
+
+            System.out.println("Turn left, ");
+
+        } else if (previousDirection == direction){
+
+            System.out.println("Continue straight, ");
+
+        }
     }
+
+
 }
